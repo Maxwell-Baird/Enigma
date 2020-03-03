d6bfb8d0796a9edb4975a53cdf5ddbefefcafbd5
Maxwell Baird <bairdm71@gmail.com>
2020-03-03 05:26:03 -0700

    made the read/write more realible

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,9 +2,10 @@ require_relative 'enigma'
 
 enigma = Enigma.new
 file_read = File.open(ARGV[0], "r")
-message = file_read.read().split("\n")
+messages = file_read.read().split("\n")
+message = messages.join(' ')
 file_read.close
-encrypted = enigma.decrypt(message[0],ARGV[2],ARGV[3])
+encrypted = enigma.decrypt(message,ARGV[2],ARGV[3])
 file_write = File.open(ARGV[1], "w")
 file_write.write(encrypted[:decryption])
 puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,9 +2,10 @@ require_relative 'enigma'
 
 enigma = Enigma.new
 file_read = File.open(ARGV[0], "r")
-message = file_read.read().split("\n")
+messages = file_read.read().split("\n")
+message = messages.join(' ')
 file_read.close
-encrypted = enigma.encrypt(message[0])
+encrypted = enigma.encrypt(message)
 file_write = File.open(ARGV[1], "w")
 file_write.write(encrypted[:encryption])
 puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
