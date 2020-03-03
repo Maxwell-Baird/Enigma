6c787bb5bc6c17bce1e421fd37c73a92fd1ff95a
Maxwell Baird <bairdm71@gmail.com>
2020-03-02 21:41:04 -0700

    created a test to check decrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -18,6 +18,17 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end
 
+  def test_it_can_decrypt
+    enigma = Enigma.new
+    expected =
+    {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
   def test_it_can_find_offset
     enigma = Enigma.new
     assert_equal 1025, enigma.offset('040895')
