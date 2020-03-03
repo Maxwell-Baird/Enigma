553b01ff50b9ce3384dda213a794726adca4b2f9
Maxwell Baird <bairdm71@gmail.com>
2020-03-02 20:18:58 -0700

    created the method find shifts which returns the four shifts

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,5 @@
 require 'date'
-
+require 'pry'
 class Enigma
 
   def encrypt(message, key=rand(10000..99999), date=Date.today)
@@ -7,7 +7,13 @@ class Enigma
   end
 
   def find_shifts(key, date)
-    offsets = offset(date)
+    shifts = []
+    offsets = offset(date).to_s
+    shifts << (key[0..1].to_i + offsets[0].to_i)
+    shifts << (key[1..2].to_i + offsets[1].to_i)
+    shifts << (key[2..3].to_i + offsets[2].to_i)
+    shifts << (key[3..4].to_i + offsets[3].to_i)
+    shifts
   end
 
   def offset(date)
