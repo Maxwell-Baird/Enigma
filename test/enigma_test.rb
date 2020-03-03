86405fafbe14f3e87a581251ce8901192d1e8d13
Maxwell Baird <bairdm71@gmail.com>
2020-03-02 19:53:24 -0700

    added the test to check if enigma can encrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,4 +7,14 @@ class EnigmaTest < Minitest::Test
     enigma = Enigma.new
     assert_instance_of Enigma, enigma
   end
+
+  def test_it_can_encrypt
+    enigma = Enigma.new
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
 end
